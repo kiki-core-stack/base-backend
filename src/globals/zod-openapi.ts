9aa4273e951ea283029dd1ea8be1e159a7827b2b
kiki-kanri <a470666@gmail.com>
2025-12-05 17:53:13 +0800

    feat: add `defineApiRouteZodOpenApiResponseConfig`  global helper

--- src/globals/zod-openapi.ts
+++ src/globals/zod-openapi.ts
@@ -1,4 +1,7 @@
+import { statusCodeToApiResponseErrorCodeMap } from '@kiki-core-stack/pack/hono-backend/constants/response';
+import type { ContentfulStatusCode } from 'hono/utils/http-status';
 import type { Except } from 'type-fest';
+import type { ZodObject } from 'zod';
 
 import type { RouteZodOpenApiConfig } from '@/core/types/zod-openapi';
 
@@ -13,5 +16,32 @@ export function defineApiRouteZodOpenApiConfig(
         description,
         operationId,
         tags: Array.isArray(tags) ? tags : [tags],
+    };
+}
+
+export function defineApiRouteZodOpenApiResponseConfig(
+    dataSchema?: ZodObject,
+    errorCodeOrStatusCode?: ContentfulStatusCode | string,
+    description?: string,
+) {
+    let schema = z.object({
+        message: z.string().optional(),
+        success: z.literal(errorCodeOrStatusCode === undefined),
+    });
+
+    if (dataSchema) schema = schema.extend({ data: dataSchema });
+    if (errorCodeOrStatusCode !== undefined) {
+        schema = schema.extend({
+            errorCode: z.literal(
+                typeof errorCodeOrStatusCode === 'number'
+                    ? statusCodeToApiResponseErrorCodeMap[errorCodeOrStatusCode]
+                    : errorCodeOrStatusCode,
+            ),
+        });
+    }
+
+    return {
+        content: { 'application/json': { schema } },
+        description: description ?? (errorCodeOrStatusCode ? '錯誤' : '成功'),
     };
 }
